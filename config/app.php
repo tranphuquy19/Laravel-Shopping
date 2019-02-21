f891817dcf8398df5d77fce67ce7a52f9c875abb
tranphuquy19 <tranphuquy19@gmail.com>
2019-02-21 23:43:51 +0700

    update

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    /*
+  /*
       |--------------------------------------------------------------------------
       | Application Name
       |--------------------------------------------------------------------------
@@ -11,8 +11,8 @@ return [
       | any other location as required by the application or its packages.
      */
 
-    'name' => 'Đồng hồ Chip Chip',
-    /*
+  'name' => 'Đồng hồ Chip Chip',
+  /*
       |--------------------------------------------------------------------------
       | Application Environment
       |--------------------------------------------------------------------------
@@ -22,8 +22,8 @@ return [
       | services your application utilizes. Set this in your ".env" file.
       |
      */
-    'env' => env('APP_ENV', 'production'),
-    /*
+  'env' => env('APP_ENV', 'production'),
+  /*
       |--------------------------------------------------------------------------
       | Application Debug Mode
       |--------------------------------------------------------------------------
@@ -33,8 +33,8 @@ return [
       | application. If disabled, a simple generic error page is shown.
       |
      */
-    'debug' => env('APP_DEBUG', true),
-    /*
+  'debug' => env('APP_DEBUG', false),
+  /*
       |--------------------------------------------------------------------------
       | Application URL
       |--------------------------------------------------------------------------
@@ -44,8 +44,8 @@ return [
       | your application so that it is used when running Artisan tasks.
       |
      */
-    'url' => env('APP_URL', 'http://localhost:8000'),
-    /*
+  'url' => env('APP_URL', 'http://localhost:8000'),
+  /*
       |--------------------------------------------------------------------------
       | Application Timezone
       |--------------------------------------------------------------------------
@@ -55,8 +55,8 @@ return [
       | ahead and set this to a sensible default for you out of the box.
       |
      */
-    'timezone' => 'UTC',
-    /*
+  'timezone' => 'UTC',
+  /*
       |--------------------------------------------------------------------------
       | Application Locale Configuration
       |--------------------------------------------------------------------------
@@ -66,8 +66,8 @@ return [
       | to any of the locales which will be supported by the application.
       |
      */
-    'locale' => 'en',
-    /*
+  'locale' => 'en',
+  /*
       |--------------------------------------------------------------------------
       | Application Fallback Locale
       |--------------------------------------------------------------------------
@@ -77,8 +77,8 @@ return [
       | the language folders that are provided through your application.
       |
      */
-    'fallback_locale' => 'en',
-    /*
+  'fallback_locale' => 'en',
+  /*
       |--------------------------------------------------------------------------
       | Encryption Key
       |--------------------------------------------------------------------------
@@ -88,9 +88,9 @@ return [
       | will not be safe. Please do this before deploying an application!
       |
      */
-    'key' => env('APP_KEY'),
-    'cipher' => 'AES-256-CBC',
-    /*
+  'key' => env('APP_KEY'),
+  'cipher' => 'AES-256-CBC',
+  /*
       |--------------------------------------------------------------------------
       | Logging Configuration
       |--------------------------------------------------------------------------
@@ -102,9 +102,9 @@ return [
       | Available Settings: "single", "daily", "syslog", "errorlog"
       |
      */
-    'log' => env('APP_LOG', 'single'),
-    'log_level' => env('APP_LOG_LEVEL', 'debug'),
-    /*
+  'log' => env('APP_LOG', 'single'),
+  'log_level' => env('APP_LOG_LEVEL', 'debug'),
+  /*
       |--------------------------------------------------------------------------
       | Autoloaded Service Providers
       |--------------------------------------------------------------------------
@@ -114,55 +114,80 @@ return [
       | this array to grant expanded functionality to your applications.
       |
      */
-    'providers' => [
-        /*
+  'providers' => [
+    /*
          * Laravel Framework Service Providers...
          */
 
-        Illuminate\Auth\AuthServiceProvider::class,
-        Illuminate\Broadcasting\BroadcastServiceProvider::class,
-        Illuminate\Bus\BusServiceProvider::class,
-        Illuminate\Cache\CacheServiceProvider::class,
-        Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
-        Illuminate\Cookie\CookieServiceProvider::class,
-        Illuminate\Database\DatabaseServiceProvider::class,
-        Illuminate\Encryption\EncryptionServiceProvider::class,
-        Illuminate\Filesystem\FilesystemServiceProvider::class,
-        Illuminate\Foundation\Providers\FoundationServiceProvider::class,
-        Illuminate\Hashing\HashServiceProvider::class,
-        Illuminate\Mail\MailServiceProvider::class,
-        Illuminate\Notifications\NotificationServiceProvider::class,
-        Illuminate\Pagination\PaginationServiceProvider::class,
-        Illuminate\Pipeline\PipelineServiceProvider::class,
-        Illuminate\Queue\QueueServiceProvider::class,
-        Illuminate\Redis\RedisServiceProvider::class,
-        Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
-        Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
-        Illuminate\Validation\ValidationServiceProvider::class,
-        Illuminate\View\ViewServiceProvider::class,
+    Illuminate\Auth\AuthServiceProvider::class,
+    Illuminate\Broadcasting\BroadcastServiceProvider::class,
+    Illuminate\Bus\BusServiceProvider::class,
+    Illuminate\Cache\CacheServiceProvider::class,
+    Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
+    Illuminate\Cookie\CookieServiceProvider::class,
+    Illuminate\Database\DatabaseServiceProvider::class,
+    Illuminate\Encryption\EncryptionServiceProvider::class,
+    Illuminate\Filesystem\FilesystemServiceProvider::class,
+    Illuminate\Foundation\Providers\FoundationServiceProvider::class,
+    Illuminate\Hashing\HashServiceProvider::class,
+    Illuminate\Mail\MailServiceProvider::class,
+    Illuminate\Notifications\NotificationServiceProvider::class,
+    Illuminate\Pagination\PaginationServiceProvider::class,
+    Illuminate\Pipeline\PipelineServiceProvider::class,
+    Illuminate\Queue\QueueServiceProvider::class,
+    Illuminate\Redis\RedisServiceProvider::class,
+    Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
+    Illuminate\Session\SessionServiceProvider::class,
+    Illuminate\Translation\TranslationServiceProvider::class,
+    Illuminate\Validation\ValidationServiceProvider::class,
+    Illuminate\View\ViewServiceProvider::class,
 
-        AvoRed\Framework\Provider::class,
-        //AvoRed\Ecommerce\Provider::class,
+    AvoRed\Framework\Provider::class,
+    //AvoRed\Ecommerce\Provider::class,
 
-        /*
+    /*
          * Package Service Providers...
          */
 
-        //AvoRed\Install\Module::class,
-        //AvoRed\Ecommerce\Provider::class,
+    //AvoRed\Install\Module::class,
+    //AvoRed\Ecommerce\Provider::class,
 
-        /*
+    /*
          * Application Service Providers...
          */
 
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        App\Providers\AvoRedConfigurationProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
+    App\Providers\AvoRedConfigurationProvider::class,
+    App\Providers\EventServiceProvider::class,
+    App\Providers\RouteServiceProvider::class,
+  ],
+
+  'debug_blacklist' => [
+    '_ENV' => [
+      'APP_KEY',
+      'DB_PASSWORD',
+      'REDIS_PASSWORD',
+      'MAIL_PASSWORD',
+      'PUSHER_APP_KEY',
+      'PUSHER_APP_SECRET',
+      // thêm key bạn cho rằng nhạy cảm vào đây
     ],
-    /*
+    '_SERVER' => [
+      'APP_KEY',
+      'DB_PASSWORD',
+      'REDIS_PASSWORD',
+      'MAIL_PASSWORD',
+      'PUSHER_APP_KEY',
+      'PUSHER_APP_SECRET',
+      // vào đây
+    ],
+    '_POST' => [
+      'password',
+      // và vào đây nữa
+    ],
+  ],
+  /*
       |--------------------------------------------------------------------------
       | Class Aliases
       |--------------------------------------------------------------------------
@@ -172,42 +197,42 @@ return [
       | the aliases are "lazy" loaded so they don't hinder performance.
       |
      */
-    'aliases' => [
-        'App' => Illuminate\Support\Facades\App::class,
-        'Artisan' => Illuminate\Support\Facades\Artisan::class,
-        'Auth' => Illuminate\Support\Facades\Auth::class,
-        'Blade' => Illuminate\Support\Facades\Blade::class,
-        'Cache' => Illuminate\Support\Facades\Cache::class,
-        'Config' => Illuminate\Support\Facades\Config::class,
-        'Cookie' => Illuminate\Support\Facades\Cookie::class,
-        'Crypt' => Illuminate\Support\Facades\Crypt::class,
-        'DB' => Illuminate\Support\Facades\DB::class,
-        'Eloquent' => Illuminate\Database\Eloquent\Model::class,
-        'Event' => Illuminate\Support\Facades\Event::class,
-        'File' => Illuminate\Support\Facades\File::class,
-        'Gate' => Illuminate\Support\Facades\Gate::class,
-        'Hash' => Illuminate\Support\Facades\Hash::class,
-        'Lang' => Illuminate\Support\Facades\Lang::class,
-        'Log' => Illuminate\Support\Facades\Log::class,
-        'Mail' => Illuminate\Support\Facades\Mail::class,
-        'Notification' => Illuminate\Support\Facades\Notification::class,
-        'Password' => Illuminate\Support\Facades\Password::class,
-        'Queue' => Illuminate\Support\Facades\Queue::class,
-        'Redirect' => Illuminate\Support\Facades\Redirect::class,
-        'Redis' => Illuminate\Support\Facades\Redis::class,
-        'Request' => Illuminate\Support\Facades\Request::class,
-        'Response' => Illuminate\Support\Facades\Response::class,
-        'Route' => Illuminate\Support\Facades\Route::class,
-        'Schema' => Illuminate\Support\Facades\Schema::class,
-        'Session' => Illuminate\Support\Facades\Session::class,
-        'Storage' => Illuminate\Support\Facades\Storage::class,
-        'URL' => Illuminate\Support\Facades\URL::class,
-        'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View' => Illuminate\Support\Facades\View::class,
+  'aliases' => [
+    'App' => Illuminate\Support\Facades\App::class,
+    'Artisan' => Illuminate\Support\Facades\Artisan::class,
+    'Auth' => Illuminate\Support\Facades\Auth::class,
+    'Blade' => Illuminate\Support\Facades\Blade::class,
+    'Cache' => Illuminate\Support\Facades\Cache::class,
+    'Config' => Illuminate\Support\Facades\Config::class,
+    'Cookie' => Illuminate\Support\Facades\Cookie::class,
+    'Crypt' => Illuminate\Support\Facades\Crypt::class,
+    'DB' => Illuminate\Support\Facades\DB::class,
+    'Eloquent' => Illuminate\Database\Eloquent\Model::class,
+    'Event' => Illuminate\Support\Facades\Event::class,
+    'File' => Illuminate\Support\Facades\File::class,
+    'Gate' => Illuminate\Support\Facades\Gate::class,
+    'Hash' => Illuminate\Support\Facades\Hash::class,
+    'Lang' => Illuminate\Support\Facades\Lang::class,
+    'Log' => Illuminate\Support\Facades\Log::class,
+    'Mail' => Illuminate\Support\Facades\Mail::class,
+    'Notification' => Illuminate\Support\Facades\Notification::class,
+    'Password' => Illuminate\Support\Facades\Password::class,
+    'Queue' => Illuminate\Support\Facades\Queue::class,
+    'Redirect' => Illuminate\Support\Facades\Redirect::class,
+    'Redis' => Illuminate\Support\Facades\Redis::class,
+    'Request' => Illuminate\Support\Facades\Request::class,
+    'Response' => Illuminate\Support\Facades\Response::class,
+    'Route' => Illuminate\Support\Facades\Route::class,
+    'Schema' => Illuminate\Support\Facades\Schema::class,
+    'Session' => Illuminate\Support\Facades\Session::class,
+    'Storage' => Illuminate\Support\Facades\Storage::class,
+    'URL' => Illuminate\Support\Facades\URL::class,
+    'Validator' => Illuminate\Support\Facades\Validator::class,
+    'View' => Illuminate\Support\Facades\View::class,
 
-        'PDF' => Barryvdh\DomPDF\Facade::class,
+    'PDF' => Barryvdh\DomPDF\Facade::class,
 
-        /*
+    /*
          *
          * @todo Remove All Facade and move it into an Framework
          * E commerce Admin Facades
@@ -217,23 +242,23 @@ return [
          *
          */
 
-        /*
+    /*
          * AvoRed Framework Facade
          *
          */
 
-        'AdminMenu' => AvoRed\Framework\AdminMenu\Facade::class,
-        'AdminConfiguration' => AvoRed\Framework\AdminConfiguration\Facade::class,
-        'Breadcrumb' => AvoRed\Framework\Breadcrumb\Facade::class,
-        'Cart' => AvoRed\Framework\Cart\Facade::class,
-        'DataGrid' => AvoRed\Framework\DataGrid\Facade::class,
-        'Image' => AvoRed\Framework\Image\Facades\Image::class,
-        'Menu' => AvoRed\Framework\Menu\Facades\Menu::class,
-        'Payment' => AvoRed\Framework\Payment\Facade::class,
-        'Permission' => AvoRed\Framework\Permission\Facade::class,
-        'Shipping' => AvoRed\Framework\Shipping\Facade::class,
-        'Tabs' => AvoRed\Framework\Tabs\Facade::class,
-        'Theme' => AvoRed\Framework\Theme\Facade::class,
-        'Widget' => AvoRed\Framework\Widget\Facade::class,
-    ],
+    'AdminMenu' => AvoRed\Framework\AdminMenu\Facade::class,
+    'AdminConfiguration' => AvoRed\Framework\AdminConfiguration\Facade::class,
+    'Breadcrumb' => AvoRed\Framework\Breadcrumb\Facade::class,
+    'Cart' => AvoRed\Framework\Cart\Facade::class,
+    'DataGrid' => AvoRed\Framework\DataGrid\Facade::class,
+    'Image' => AvoRed\Framework\Image\Facades\Image::class,
+    'Menu' => AvoRed\Framework\Menu\Facades\Menu::class,
+    'Payment' => AvoRed\Framework\Payment\Facade::class,
+    'Permission' => AvoRed\Framework\Permission\Facade::class,
+    'Shipping' => AvoRed\Framework\Shipping\Facade::class,
+    'Tabs' => AvoRed\Framework\Tabs\Facade::class,
+    'Theme' => AvoRed\Framework\Theme\Facade::class,
+    'Widget' => AvoRed\Framework\Widget\Facade::class,
+  ],
 ];
--- modules/avored/feature/resources/views/widget/index.blade.php
+++ modules/avored/feature/resources/views/widget/index.blade.php
@@ -1,7 +1,7 @@
 
 <div class="row">
     <div class="col-md-12">
-        <div class="h2">Chức năng sản phẩm</div>
+        <div class="h2">Sản phẩm</div>
     </div>
 </div>
 
