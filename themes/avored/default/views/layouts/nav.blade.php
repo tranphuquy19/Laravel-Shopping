96631851ad9204658997580749e7582cd8b2a782
tranphuquy19 <tranphuquy19@gmail.com>
2019-02-22 00:24:40 +0700

    update

--- themes/avored/default/views/layouts/nav.blade.php
+++ themes/avored/default/views/layouts/nav.blade.php
@@ -70,7 +70,7 @@
             <div class="col-md-6">
                 <a class="navbar-brand" href="{{ route('home') }}">
                     <span style="font-size: 32px; font-weight: 500; margin-top: 10px;">
-                        <img src="{{ asset('vendor/avored-default/images/logo.svg') }}" alt="logo" class="logo">SmartWatch Store
+                        <img src="{{ asset('vendor/avored-default/images/logo.svg') }}" alt="logo" class="logo">ChipChipWatch Store
                     </span>
                 </a>
             </div>
